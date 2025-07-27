452f049e19a8f48b238ae7de7d39089606b1e43a
Nitesh Singh <niteshsingh1705@gmail.com>
2025-07-27 23:19:12 +0530

    make responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ import { useState } from "react";
 export default function Home() {
   const [files, setFiles] = useState<FileWithPreview[]>([]);
   return (
-    <div className="flex items-center justify-items-center">
-      <div className="flex w-full items-center justify-center gap-2">
+    <div className="flex p-4 lg:p-0 items-center justify-items-center">
+      <div className="flex flex-col lg:flex-row flex-wrap w-full items-center justify-center gap-2">
         <div
           className={clsx(
-            "sm:p-20 w-full min-h-screen flex flex-col items-center justify-center gap-2",
+            "sm:p-20 w-full  lg:min-h-screen flex flex-col items-center justify-center gap-2",
             {
               "w-2/3": files.length > 0,
             }
@@ -26,7 +26,7 @@ export default function Home() {
           />
         </div>
         {files.length > 0 && (
-          <div className="bg-gray-50 w-1/3">
+          <div className="bg-gray-50 w-full lg:w-1/3">
             <ResizeImage files={files} />
           </div>
         )}
